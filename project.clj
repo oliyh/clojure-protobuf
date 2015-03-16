1e3fe8a703ca14870f99b5b833a43bb81386bbde
Justin Balthrop <git@justinbalthrop.com>
2015-03-16 08:54:27 -0700

    update lein-protobuf

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/protobuf "0.8.2-SNAPSHOT"
+(defproject org.flatland/protobuf "0.8.2"
   :description "Clojure-protobuf provides a clojure interface to Google's protocol buffers."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -8,7 +8,7 @@
                  [org.flatland/schematic "0.1.0"]
                  [org.flatland/io "0.3.0"]
                  [ordered-collections "0.4.0"]]
-  :plugins [[lein-protobuf "0.4.1"]]
+  :plugins [[lein-protobuf "0.4.2"]]
   :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.5,default" "test"]}
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
